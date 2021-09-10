bb29f559d80e03e76b287afa5bd48d483ce75b94
gabitheiss <t_gaaby@hotmail.com>
2021-09-10 15:04:05 -0300

    View model com injeção do repository

--- app/src/main/java/com/example/diffutils_concatadapter_retrofit/view_model/MainViewModel.kt
+++ app/src/main/java/com/example/diffutils_concatadapter_retrofit/view_model/MainViewModel.kt
@@ -1,7 +1,28 @@
 package com.example.diffutils_concatadapter_retrofit.view_model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.diffutils_concatadapter_retrofit.model.Image
+import com.example.diffutils_concatadapter_retrofit.repository.PixabayRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class MainViewModel @Inject constructor(private val repository : PixabayRepository) : ViewModel() {
+
+
+    private val _images = MutableLiveData<List<Image>>()
+    val images : LiveData<List<Image>> = _images
+
+    fun fetchImages(q: String = "") {
+        viewModelScope.launch {
+            val returnedImages = repository.fetchImages(q = q)
+            returnedImages?.let {
+                _images.value = it
+            }
+        }
+    }
 }
