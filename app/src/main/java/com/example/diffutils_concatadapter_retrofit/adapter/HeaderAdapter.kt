4fc1db0e86d2c301156b3352a656c6e14f97d77e
gabitheiss <t_gaaby@hotmail.com>
2021-09-13 09:11:05 -0300

    Adapters

--- app/src/main/java/com/example/diffutils_concatadapter_retrofit/adapter/HeaderAdapter.kt
+++ app/src/main/java/com/example/diffutils_concatadapter_retrofit/adapter/HeaderAdapter.kt
@@ -2,25 +2,27 @@ package com.example.diffutils_concatadapter_retrofit.adapter
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.diffutils_concatadapter_retrofit.R
 import com.example.diffutils_concatadapter_retrofit.databinding.HeaderSearchBinding
 
 
 class HeaderAdapter(val onClick:(String)->Unit):RecyclerView.Adapter<HeaderViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeaderViewHolder {
-        TODO("Not yet implemented")
+        LayoutInflater.from(parent.context).inflate(R.layout.header_search, parent, false).apply {
+            return HeaderViewHolder(this, onClick)
+        }
     }
 
     override fun onBindViewHolder(holder: HeaderViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind()
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = 1
 
 }
 
